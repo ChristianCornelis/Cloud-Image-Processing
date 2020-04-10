95b1ca14f5b04a58509810684766058e4448b84f
Mohammadamin Sheikhtaheri <msheikht@uoguelph.ca>
2020-04-10 15:34:58 -0400

    Committing AWS Rekognition API calls

---
+++ api_modules/rekognition_detect.py
@@ -0,0 +1,23 @@
+import boto3
+import os
+import io
+
+def call_rekognition():
+
+    client = boto3.client('rekognition')
+    file = open('aws_response.txt', "w")
+
+    directory = './resources/'
+
+    for filename in os.listdir(directory):
+        with io.open(os.path.join('resources', filename), 'rb') as image_file:
+            content = image_file.read()
+            response = client.detect_labels(Image={"Bytes":content})
+            file.write('Detected labels for ' + filename + '\n') 
+            for label in response['Labels']:
+                file.write("    " + label['Name'] + " (" + str("%.2f" % label['Confidence']) + "% Confidence)" + '\n')
+                if(label['Parents']):
+                    file.write("       Parents:\n")
+                for parent in label['Parents']:
+                    file.write("           " + parent['Name'] + '\n')
+    file.close()
--- driver.py
+++ driver.py
@@ -1,2 +1,4 @@
 from api_modules import gcp_vision_api
+from api_modules import rekognition_detect
 gcp_vision_api.call_vision()
+rekognition_detect.call_rekognition()
