271ea2e5445127e2f0c5b9bc770af00ee7d57a8c
Samuel Tracz <traczs@uoguelph.ca>
2020-04-10 16:51:01 -0400

    adding azure

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # CIS 4010 Final Project
-## Christian Cornelis (0939357), Mohammadamin Sheikhtaheri (0930853), Samuel Tracz ()
+## Christian Cornelis (0939357), Mohammadamin Sheikhtaheri (0930853), Samuel Tracz (0927451)
 
 ## Requirements
 
@@ -16,3 +16,12 @@
 - You must have the service account key for the above project set in your environment variable `GOOGLE_APPLICATION_CREDENTIALS`
 - You must have installed the Python module `google-cloud-vision` via PIP using `pip install google-cloud-vision`
 - You can follow [this](https://cloud.google.com/vision/docs/setup#windows) link for helpful steps for setting up the above
+
+### Azure Computer Vision
+- You must have an azure account
+- you must have a a subscription key for Computer Vision. (https://azure.microsoft.com/try/cognitive-services/?api=computer-vision)
+- requres these python libraries:
+    requests
+    matplotlib
+    pillow
+- go to (https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/quickstarts/python-disk) for setup steps and code tutorial
---
+++ api_modules/azure-analyze-local-image.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import requests
+# If you are using a Jupyter notebook, uncomment the following line.
+# %matplotlib inline
+import matplotlib.pyplot as plt
+from PIL import Image
+from io import BytesIO
+import time
+
+def az_vision():
+    # Add your Computer Vision subscription key and endpoint to your environment variables.
+    if 'COMPUTER_VISION_SUBSCRIPTION_KEY' in os.environ:
+        subscription_key = os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY']
+    else:
+        print("\nSet the COMPUTER_VISION_SUBSCRIPTION_KEY environment variable.\n**Restart your shell or IDE for changes to take effect.**")
+        sys.exit()
+
+    if 'COMPUTER_VISION_ENDPOINT' in os.environ:
+        endpoint = os.environ['COMPUTER_VISION_ENDPOINT']
+
+    #analyze_url = endpoint + "/v2.1/analyze"
+    tag_url = endpoint + "/v2.1/tag"
+
+
+    directory = './resources/'
+
+    for filename in os.listdir(directory):
+
+        # Set image_path to the local path of an image that you want to analyze.
+
+        
+        # Read the image into a byte array
+        print(filename)
+        image_data = open(os.path.join('resources', filename), 'rb').read()
+        headers = {'Ocp-Apim-Subscription-Key': subscription_key,
+                'Content-Type': 'application/octet-stream'}
+        params = {'visualFeatures': 'Categories,Description,Color'}
+        #response = requests.post(
+        #    analyze_url, headers=headers, params=params, data=image_data)
+        #response.raise_for_status()
+
+        response2 = requests.post(
+            tag_url, headers=headers, params=params, data=image_data)
+        response2.raise_for_status()
+
+        # The 'analysis' object contains various fields that describe the image. The most
+        # relevant caption for the image is obtained from the 'description' property.
+        #analysis = response.json()
+        analysis2 = response2.json()
+        for label in analysis2['tags']:
+            print('\t' + label['name'] + ' (' + ("%.2f" % (label['confidence'] * 100)) +'% confidence)')
+        print("")
+        #print(analysis2)
+        #image_caption = analysis["description"]["captions"][0]["text"].capitalize()
+
+        # Display the image and overlay it with the caption.
+        #image = Image.open(BytesIO(image_data))
+        #plt.imshow(image)
+        #plt.axis("off")
+        #_ = plt.title(image_caption, size="x-large", y=-0.1)
+        #plt.show()
+        time.sleep(15)
