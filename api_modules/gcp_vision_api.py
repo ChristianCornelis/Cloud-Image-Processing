fd3725d685ef0ebbdbdba9ad6569c38b5d687195
ChristianCornelis <ccorneli@uoguelph.ca>
2020-04-10 14:45:00 -0400

    Created driver program to run all APIs

--- api_modules/gcp_vision_api.py
+++ api_modules/gcp_vision_api.py
@@ -5,23 +5,23 @@ import os
 from google.cloud import vision
 from google.cloud.vision import types
 
+def call_vision():
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
+    directory = './resources/'
 
-directory = './resources/'
+    # Iterate over each image in the resources directory, outputting labels for each.
+    for filename in os.listdir(directory):
+        with io.open(os.path.join('resources', filename), 'rb') as image_file:
+            content = image_file.read()
 
-# Iterate over each image in the resources directory, outputting labels for each.
-for filename in os.listdir(directory):
-    with io.open(os.path.join('resources', filename), 'rb') as image_file:
-        content = image_file.read()
+            image = types.Image(content=content)
 
-        image = types.Image(content=content)
+            # Performs label detection on the image file
+            response = client.label_detection(image=image)
+            labels = response.label_annotations
 
-        # Performs label detection on the image file
-        response = client.label_detection(image=image)
-        labels = response.label_annotations
-
-        print(filename + ' Labels:')
-        for label in labels:
-            print('\t' + label.description + ' (' + ("%.2f" % (label.score * 100)) +'% confidence)')
+            print(filename + ' Labels:')
+            for label in labels:
+                print('\t' + label.description + ' (' + ("%.2f" % (label.score * 100)) +'% confidence)')
